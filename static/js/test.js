6d9aed0ce8d50ae579fb8ec4738acc7bea6ec114
YemingD <159274326+YemingD@users.noreply.github.com>
2024-07-21 13:28:28 +0200

    Add files via upload

---
+++ static/js/test.js
@@ -0,0 +1,12 @@
+function PageMap(index) {
+    let mp={
+        "1":['Library information management'],
+        "2":['Warehouse management'],
+        "3":['Order review'],
+        "4":['Book purchase'],
+        "5":['Invoice management'],
+        "6":['Classified management'],
+    }
+
+    return mp[index];
+}
